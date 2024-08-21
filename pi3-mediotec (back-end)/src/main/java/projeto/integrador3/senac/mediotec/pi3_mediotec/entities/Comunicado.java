5b82f0d3612da1b0e1ff75e7f4f2578b57a8f64e
rafaeldgdf <rafaeldgdf@hotmail.com>
2024-08-21 11:14:08 -0300

    adicionando comunicado

--- pi3-mediotec (back-end)/src/main/java/projeto/integrador3/senac/mediotec/pi3_mediotec/entities/Comunicado.java
+++ pi3-mediotec (back-end)/src/main/java/projeto/integrador3/senac/mediotec/pi3_mediotec/entities/Comunicado.java
@@ -1,40 +1,38 @@
 package projeto.integrador3.senac.mediotec.pi3_mediotec.entities;
 
 import java.io.Serializable;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Data
-@NoArgsConstructor
 @Entity
-@Table(name = "coordenacao")
-public class Coordenacao implements Serializable {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comunicado")
+public class Comunicado implements Serializable {
 	
-    private static final long serialVersionUID = 1L;
-    
-    @Id
-    private Long id_coordenacao;
-    
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long id;
     
     @NotNull(message = "{coordenacao.nome.notnull}")
     @Size(min = 3, max = 50, message = "{coordenacao.nome.size}")
     @Column(nullable = false)
-    private String nome;
+    private String conteudo;
     
-    @Size(max = 50)
-    @Column
-    private String descricao;
     
-    @OneToOne 
-    private Coordenador coordenador;
+	
 
 }
